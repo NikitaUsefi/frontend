750da47bc9cf0f4a70afd2323384489b4e441783
nikitausefi <Nikitausefi@gmail.com>
2021-01-11 20:48:30 +0330

    Understanding Routing part 1-2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
+import {Routes,RouterModule} from "@angular/router";
+
 
 import { AppComponent } from './app.component';
 import { ProductCardComponent } from './property/property-card/property-card.component';
 import { PropertyListComponent } from './property/property-list/property-list.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HousingService } from './services/housing.service';
+import { AddPropertyComponent } from './property/add-property/add-property.component';
+import { __importDefault } from 'tslib';
+import { PropertyDetailComponent } from './property/property-detail/property-detail.component';
 
+const appRoutes:Routes=[
+  {path:'',component:PropertyListComponent},
+  {path:'rent-property',component:PropertyListComponent},
+  {path:'add-property',component:AddPropertyComponent},
+  {path:'property-detail/:id',component:PropertyDetailComponent},
 
+]
 @NgModule({
   declarations: [
     AppComponent,
     ProductCardComponent,
+    PropertyDetailComponent,
     PropertyListComponent,
-    NavBarComponent
+    AddPropertyComponent,
+    NavBarComponent,
    ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     HousingService
