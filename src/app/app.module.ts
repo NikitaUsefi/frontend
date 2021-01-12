627e0ebd9732879b09bf9efb0daef8db2c11c5c2
nikitausefi <Nikitausefi@gmail.com>
2021-01-12 20:46:31 +0330

    Template Driven Forms in Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {Routes,RouterModule} from "@angular/router";
-
+import {FormsModule} from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { ProductCardComponent } from './property/property-card/property-card.component';
@@ -35,7 +35,8 @@ const appRoutes:Routes=[
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    FormsModule
   ],
   providers: [
     HousingService
