141bd0bf4f310b587a131910faeeb529868591da
nikitausefi <Nikitausefi@gmail.com>
2021-01-13 11:13:55 +0330

    Reactive Forms in angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
-import {Routes,RouterModule} from "@angular/router";
-import {FormsModule} from "@angular/forms";
+import { HttpClientModule } from '@angular/common/http';
+import { Routes, RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductCardComponent } from './property/property-card/property-card.component';
@@ -13,15 +13,18 @@ import { AddPropertyComponent } from './property/add-property/add-property.compo
 import { __importDefault } from 'tslib';
 import { PropertyDetailComponent } from './property/property-detail/property-detail.component';
 import { PageNotFoundComponent } from './Page-Not-Found/Page-Not-Found.component';
+import { UserLoginComponent } from './user/user-login/user-login.component';
+import { UserRegisterComponent } from './user/user-register/user-register.component';
 
-const appRoutes:Routes=[
-  {path:'',component:PropertyListComponent},
-  {path:'rent-property',component:PropertyListComponent},
-  {path:'add-property',component:AddPropertyComponent},
-  {path:'property-detail/:id',component:PropertyDetailComponent},
-  {path:'**',component:PageNotFoundComponent}
-
-]
+const appRoutes: Routes = [
+  { path: '', component: PropertyListComponent },
+  { path: 'rent-property', component: PropertyListComponent },
+  { path: 'add-property', component: AddPropertyComponent },
+  { path: 'property-detail/:id', component: PropertyDetailComponent },
+  { path: 'user/login', component:UserLoginComponent},
+  { path: 'user/register', component:UserRegisterComponent},
+  { path: '**', component: PageNotFoundComponent },
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -30,17 +33,18 @@ const appRoutes:Routes=[
     PropertyListComponent,
     AddPropertyComponent,
     NavBarComponent,
-      PageNotFoundComponent
-   ],
+    PageNotFoundComponent,
+    UserLoginComponent,
+    UserRegisterComponent
+  ],
   imports: [
     BrowserModule,
     HttpClientModule,
     RouterModule.forRoot(appRoutes),
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [
-    HousingService
-  ],
-  bootstrap: [AppComponent]
+  providers: [HousingService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
