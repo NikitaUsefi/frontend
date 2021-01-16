cd73e750f6bae1ea7b88bf3dbcaaaacafc1bd40f
nikitausefi <Nikitausefi@gmail.com>
2021-01-16 12:20:55 +0330

    Create Property Detail View

--- src/app/services/housing.service.ts
+++ src/app/services/housing.service.ts
@@ -9,40 +9,63 @@ import { IProperty } from '../model/iproperty';
 })
 export class HousingService {
   constructor(private http: HttpClient) {}
-
-  getAllProperties(SellRent: number): Observable<IProperty[]> {
+  getProperty(id: number) {
+    return this.getAllProperties().pipe(
+      map(propertiesArray=>{
+        return propertiesArray.find(p=>p.Id===id);
+      })
+    );
+  }
+  getAllProperties(SellRent?: number): Observable<Property[]> {
     return this.http.get('data/properties.json').pipe(
       map((data) => {
-        const propertiesArray: Array<IProperty> = [];
-        const localProperties=JSON.parse(localStorage.getItem('newProp'));
+        const propertiesArray: Array<Property> = [];
+        const localProperties = JSON.parse(localStorage.getItem('newProp'));
 
-        if(localProperties){
+        if (localProperties) {
           for (const id in localProperties) {
-            if (localProperties.hasOwnProperty(id) && localProperties[id].SellRent === SellRent)
-              propertiesArray.push(localProperties[id]);
+            //if we want to filter our properties by sellRent
+
+            if(SellRent){
+              if (
+                localProperties.hasOwnProperty(id) &&
+                localProperties[id].SellRent === SellRent
+              )
+                propertiesArray.push(localProperties[id]);
+            }
+              //if we want all the data
+            else propertiesArray.push(localProperties[id]);
           }
         }
         for (const id in data) {
-          if (data.hasOwnProperty(id) && data[id].SellRent === SellRent)
+          //if we want to filter our properties by sellRent
+         if(SellRent){
+          if (
+            data.hasOwnProperty(id) &&
+            data[id].SellRent === SellRent
+          )
             propertiesArray.push(data[id]);
+         }
+             //if we want all the data
+          else propertiesArray.push(data[id]);
         }
         return propertiesArray;
       })
     );
   }
   addProperty(property: Property) {
-    let newProp=[property];
-    if(localStorage.getItem('newProp')){
-      newProp=[property,...JSON.parse(localStorage.getItem('newProp'))];
+    let newProp = [property];
+    if (localStorage.getItem('newProp')) {
+      newProp = [property, ...JSON.parse(localStorage.getItem('newProp'))];
     }
     localStorage.setItem('newProp', JSON.stringify(newProp));
   }
-  newPropID(){
-    let PID='101';
-    if(localStorage.getItem('PID'))
-      PID=String(+localStorage.getItem('PID')+1);
+  newPropID() {
+    let PID = '101';
+    if (localStorage.getItem('PID'))
+      PID = String(+localStorage.getItem('PID') + 1);
 
-    localStorage.setItem('PID',PID);
+    localStorage.setItem('PID', PID);
     return PID;
   }
 }
